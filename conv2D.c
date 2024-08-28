b000994650cf3c8902e7d302dd0387864a70cf56
Rahul <47021075+rahul-siva-prasad@users.noreply.github.com>
2024-08-28 21:22:51 +0530

    stride-added

--- conv2D.c
+++ conv2D.c
@@ -9,7 +9,7 @@ typedef struct S_CONV_2D_DATA{
     int array_size;
 }CONV_2D_DATA;
 
-CONV_2D_DATA* conv2D(int* image_arr, int image_size, int* kernel, int kernel_size,int bias);
+CONV_2D_DATA* conv2D(int* image_arr, int image_size, int* kernel, int kernel_size,int stride,int bias);
 
 int image_arr[4][4] = {
     {1,   2,  3,  4},
@@ -18,39 +18,52 @@ int image_arr[4][4] = {
     {13, 14, 15, 16}
 };
 
-int kernel[3][3] = {
+int kernel[2][2] ={
+    {1, 1},
+    {1, 1}
+};
+/*int kernel[3][3] = {
     {0, -1,  0},
     {-1, 5, -1},
     {0, -1,  0}
-};
+};*/
 
-CONV_2D_DATA* conv2D(int* image_arr, int image_size, int* kernel, int kernel_size,int bias) {
-    int output_size = OUTPUT_KERNEL_SIZE(image_size,kernel_size,0,1);
+CONV_2D_DATA* conv2D(int* image_arr, int image_size, int* kernel, int kernel_size,int stride,int bias) {
+    int output_size = OUTPUT_KERNEL_SIZE(image_size,kernel_size,0,stride);
 
     CONV_2D_DATA* ptr = malloc(sizeof(CONV_2D_DATA));
     ptr->conv2D_array = malloc(sizeof(int)*output_size * output_size);
     ptr->array_size = output_size;
+    int output_index =0;
 
-    for(int row_image=0;row_image<output_size;row_image++){
-        for(int col_image=0;col_image<output_size;col_image++){
-            int indx=0;
+    for(int row_image=0;row_image<output_size+stride-1;row_image+=stride){
+        for(int col_image=0;col_image<output_size+stride-1;col_image+=stride){
             int convolved_value =0;
             for(int row_kernel = 0;row_kernel < kernel_size;row_kernel++){
                 int row_start = row_image+row_kernel;
                 for(int col_kernel = 0;col_kernel < kernel_size;col_kernel++){
-                    int col_start = col_image + col_kernel;
-                    int image_cell_val = image_arr[image_size * row_start+col_start];
-                    int kernel_cell_val = kernel[indx++];
-                    convolved_value += (image_cell_val*kernel_cell_val);
+                    int col_start       = col_image + col_kernel;
+                    int image_cell_val  = image_arr [row_start  * image_size  + col_start];
+                    int kernel_cell_val = kernel    [row_kernel * kernel_size + col_kernel];
+                    convolved_value     += (image_cell_val*kernel_cell_val);
                 }
             }
-            ptr->conv2D_array[row_image*output_size + col_image] = convolved_value + bias;
+            ptr->conv2D_array[output_index++] = convolved_value + bias;
         }
     }
+    int max_buffer =0;
     for(int row=0;row<output_size;row++){
+        int current_buffer=0;
         for(int col=0;col<output_size;col++){
-            printf("|\t%d\t|",ptr->conv2D_array[row*output_size+col]);
+            current_buffer += printf("|\t%d\t|",ptr->conv2D_array[row*output_size+col]);
+            current_buffer += 5*2;
         }
+        if(current_buffer> max_buffer)
+            max_buffer = current_buffer;
+        int temp = max_buffer;
+        printf("\n");
+        while(temp--)
+            printf("-");
         printf("\n");
     }
     return ptr;
@@ -59,8 +72,9 @@ CONV_2D_DATA* conv2D(int* image_arr, int image_size, int* kernel, int kernel_siz
 int main() {
     int image_size = ARRAY_2D_SIZE(image_arr);
     int kernel_size = ARRAY_2D_SIZE(kernel);   
-    int bias=5;
-    CONV_2D_DATA*ptr = conv2D((int*)image_arr, image_size, (int*)kernel, kernel_size,bias);
+    int bias=0;//5
+    int stride =1;//1
+    CONV_2D_DATA*ptr = conv2D((int*)image_arr, image_size, (int*)kernel, kernel_size,stride,bias);
     free(ptr->conv2D_array);
     free(ptr);
     return 0;
